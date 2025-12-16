332768ab566f91d0e66a9d43c3fb810098b0c9d9
you <you@example.com>
2025-12-16 18:39:01 +0100

    Delete and create

--- Hello V2.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Hello Window")
-label = tk.Label(root, text="Hello World!", font=("Arial", 20))
-label.pack(padx=20, pady=20)
-
-root.mainloop()
--- README.md
+++
@@ -1,1 +0,0 @@
-This repository is for teaching the basics of GitHub.
